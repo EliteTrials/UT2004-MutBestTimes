9ea811e23b095b9cb78b56d1629199705e8d4c3e
EliotVU <eliot_van@hotmail.com>
2014-01-27 02:43:39 +0100

    Updated batches.

---
+++ GroupTrialsServer.bat
@@ -0,0 +1,5 @@
+cd ..
+cd System
+ucc.exe server GTR-GeometryBasics.ut2?game=UT04TrialGame.UT04Trial_SoloGameInfo?Mutator=ServerBTimes.MutBestTimes,MutNoAutoFireV1I_Test5.MutNoAutoFire,utcompv17a.MutUTComp -log=Server.log -LAN
+
+pause
