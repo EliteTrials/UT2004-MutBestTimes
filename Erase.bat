5901eba9cdbd20ff14098610e13469ab83b6a93d
Eliot <eliot_van@hotmail.com>
2018-12-15 18:47:45 +0100

    Cleanup batch files.

--- Erase.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-for %%* in (.) do set project_name=%%~n*
-
-cd ..
-cd System
-del %project_name%.u
-del %project_name%.ucl
-del %project_name%.int
--- Server.bat
+++
@@ -1,5 +0,0 @@
-cd ..
-cd System
-ucc.exe server STR-Eliot'sTrial?game=UT04TrialGame.UT04Trial_SoloGameInfo?Mutator=ServerBTimes.MutBestTimes,MutNoAutoFireV1I_Test5.MutNoAutoFire,utcompv17a.MutUTComp?NumBots=0?QuickStart=1 -log=Server.log -LAN
-
-pause
