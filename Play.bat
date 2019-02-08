0183799b1f0e6134ba9b47415db7f3ce7cd623fd
Eliot <eliot_van@hotmail.com>
2019-02-08 20:54:26 +0100

    Let task determine the map.

--- Play.bat
+++ Play.bat
@@ -1,3 +1,3 @@
 cd ..
 cd System
-ut2004.exe STR-TechChallenges?game=UT04TrialGame.UT04Trial_SoloGameInfo?Mutator=ServerBTimes.MutBestTimes,MutNoAutoFireV1I_Test5.MutNoAutoFire,utcompv17a.MutUTComp?SpectatorOnly=0?NumBots=0?NoBots=1?QuickStart=1 -norunaway
+ut2004.exe %1?game=UT04TrialGame.UT04Trial_SoloGameInfo?Mutator=ServerBTimes.MutBestTimes,MutNoAutoFireV1I_Test5.MutNoAutoFire,utcompv17a.MutUTComp?SpectatorOnly=0?NumBots=0?NoBots=1?QuickStart=1 -norunaway
