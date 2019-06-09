d2b6cfc453773361fd8135fd8c40fd68bf2785ac
Eliot <eliot_van@hotmail.com>
2019-06-09 17:15:02 +0200

    Test on STR-TechChallenges.

--- Scripts/Server.bat
+++ Scripts/Server.bat
@@ -1,4 +1,4 @@
 cd ..\..\System
-ucc.exe server STR-Eliot'sTrial?game=UT04TrialGame.UT04Trial_SoloGameInfo?Mutator=ServerBTimes.MutBestTimes,MutNoAutoFireV1I_Test5.MutNoAutoFire,utcompv17a.MutUTComp?NumBots=0?QuickStart=1?AdminName=Eliot?AdminPassword=Test?AllowThrowing=0?AllowBehindview=1 -log=Server.log -LAN -NORUNAWAY
+ucc.exe server STR-TechChallenges?game=UT04TrialGame.UT04Trial_SoloGameInfo?Mutator=ServerBTimes.MutBestTimes,MutNoAutoFireV1I_Test5.MutNoAutoFire,utcompv17a.MutUTComp?NumBots=0?QuickStart=1?AdminName=Eliot?AdminPassword=Test?AllowThrowing=0?AllowBehindview=1 -log=Server.log -LAN -NORUNAWAY
 
 pause
